e8a3d98bdf502e7a2282ca1bb15451fa7a56577a
Brice Owen INDRIANARISON <indrianarison.brice@gmail.com>
2023-09-09 01:49:16 +0200

    Prisma DB & Categories

--- src/app/(root)/(routes)/page.tsx
+++ src/app/(root)/(routes)/page.tsx
@@ -1,9 +1,14 @@
+import prismadb from '@/lib/prismadb'
+import { Categories } from '@/components/categories'
 import { SearchInput } from '@/components/search-input'
 
-export default function HomePage() {
+export default async function HomePage() {
+  const categories = await prismadb.category.findMany()
+
   return (
     <div className='h-full p-4 space-y-2'>
       <SearchInput />
+      <Categories data={categories} />
     </div>
   )
 }
