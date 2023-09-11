f1ee3d8dd96808c58d0df12ae28d65302776d4dd
Brice Owen INDRIANARISON <indrianarison.brice@gmail.com>
2023-09-11 02:37:37 +0200

    Companions List

--- src/app/(root)/(routes)/page.tsx
+++ src/app/(root)/(routes)/page.tsx
@@ -1,14 +1,40 @@
 import prismadb from '@/lib/prismadb'
 import { Categories } from '@/components/categories'
 import { SearchInput } from '@/components/search-input'
+import Companions from '@/components/companions'
 
-export default async function HomePage() {
+interface RootPageProps {
+  searchParams: {
+    categoryId: string
+    name: string
+  }
+}
+
+export default async function RootPage({ searchParams }: RootPageProps) {
+  const data = await await prismadb.companion.findMany({
+    where: {
+      categoryId: searchParams.categoryId,
+      name: searchParams.name,
+    },
+    orderBy: {
+      createdAt: 'desc',
+    },
+    include: {
+      _count: {
+        select: {
+          messages: true,
+        },
+      },
+    },
+  })
+
   const categories = await prismadb.category.findMany()
 
   return (
     <div className='h-full p-4 space-y-2'>
       <SearchInput />
       <Categories data={categories} />
+      <Companions data={data} />
     </div>
   )
 }
